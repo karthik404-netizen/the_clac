a7987932ac56a17d7f2a0c01981201fe5871cf97
Karhik.K <karthikk152004@gmail.com>
2025-04-19 10:39:46 +0530

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,78 @@
+public class Main {
+    public static void main(String[] args){
+        String str= t1.getText();
+    String st=t2.getText();
+    String sc=t3.getText();
+
+    double n1,n2,n3;
+    n1=Double.parseDouble(t1.getText());
+    n2=Double.parseDouble(t2.getText());
+    n3=n1+n2;
+    t3.setText(Double.toString(n3));
+
+
+    String str= t1.getText();
+    String st=t2.getText();
+    String sc=t3.getText();
+
+    double n1,n2,n3;
+    n1=Double.parseDouble(t1.getText());
+    n2=Double.parseDouble(t2.getText());
+    n3=n1-n2;
+    t3.setText(Double.toString(n3));
+
+
+    String str= t1.getText();
+    String st=t2.getText();
+    String sc=t3.getText();
+
+    double n1,n2,n3;
+    n1=Double.parseDouble(t1.getText());
+    n2=Double.parseDouble(t2.getText());
+    n3=n1*n2;
+    t3.setText(Double.toString(n3));
+
+
+    String str= t1.getText();
+    String st=t2.getText();
+    String sc=t3.getText();
+
+    double n1,n2,n3;
+    n1=Double.parseDouble(t1.getText());
+    n2=Double.parseDouble(t2.getText());
+    n3=n1/n2;
+    t3.setText(Double.toString(n3));
+
+    String str= t1.getText();
+    String st=t2.getText();
+    String sc=t3.getText();
+
+    double n1,n2,n3;
+    n1=Double.parseDouble(t1.getText());
+    n2=Double.parseDouble(t2.getText());
+    n3=n1%n2;
+    t3.setText(Double.toString(n3));  
+
+    t1.setText("");
+    t2.setText("");
+    t3.setText("");
+     
+
+    System.exit(0);
+    
+    
+
+    
+
+    
+
+    
+
+    
+
+  
+
+      
+
+    }  
+}
